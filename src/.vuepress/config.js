eabe9767d153b68c65cb855f35dcc7ac50951c74
Cody Bontecou <bontecouc@gmail.com>
2021-07-04 22:39:15 -1000

    viewport change for firefox

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,6 +10,10 @@ module.exports = {
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
     ],
+    [
+      'meta',
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+    ],
   ],
   themeConfig: {
     repo: '',
