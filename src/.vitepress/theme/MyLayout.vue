397fad0dd0d40eb42fe9586fe6139b519df2e8cf
Cody Bontecou <bontecouc@gmail.com>
2022-08-07 19:27:25 -1000

    Revert "Attempting to watch route to force update post"
    This reverts commit 132bdbfe53ff2873e89a4b2810c9fc62c2381102.

--- src/.vitepress/theme/MyLayout.vue
+++ src/.vitepress/theme/MyLayout.vue
@@ -1,29 +1,14 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
-import { useRoute } from 'vitepress'
-import { ref, watch, getCurrentInstance } from 'vue'
 
 const { Layout } = DefaultTheme
-const route = useRoute()
-const currentRoute = ref(route)
-const instance = getCurrentInstance()
-
-watch(route, (newRoute, oldRoute) => {
-  currentRoute.value = newRoute.path
-  console.log(currentRoute.value)
-  instance?.proxy?.$forceUpdate()
-})
 </script>
 
 <template>
   <Layout>
     <template #doc-after>
       <SimpleNewsletter />
-      <Post
-        repo="CodyBontecou/blog"
-        theme="github-dark"
-        :issue-term="currentRoute.value"
-      />
+      <Post repo="CodyBontecou/blog" theme="github-dark" />
     </template>
   </Layout>
 </template>
