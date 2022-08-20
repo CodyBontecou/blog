3f39755e0b40d7e960f73d88c0a5d712563f8ce7
Cody Bontecou <bontecouc@gmail.com>
2022-08-19 22:30:24 -1000

    trying new comments

--- src/.vitepress/theme/MyLayout.vue
+++ src/.vitepress/theme/MyLayout.vue
@@ -1,26 +1,41 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import { useRoute } from 'vitepress'
-import { ref, watch } from 'vue'
+// import { useScriptTag } from '@vueuse/core'
+import { computed, ref, watch, onMounted } from 'vue'
 
 const { Layout } = DefaultTheme
 const route = useRoute()
 const currentRoute = ref(route)
+// const comment = ref('comment')
 
 watch(route, (newRoute, oldRoute) => {
   currentRoute.value = newRoute.path
 })
+
+// useScriptTag(
+//   'https://utteranc.es/client.js',
+//   e => {
+//     comment.value = e
+//     console.log(comment.value)
+//   },
+//   {
+//     async: true,
+//     crossOrigin: 'anonymous',
+//     options: {
+//       repo: 'codybontecou/blog',
+//       theme: 'github-dark',
+//       'issue-term': currentRoute.value.path,
+//     },
+//   }
+// )
 </script>
 
 <template>
   <Layout>
     <template #doc-after>
       <SimpleNewsletter />
-      <Comments
-        repo="CodyBontecou/blog"
-        theme="github-dark"
-        :issue-term="currentRoute.value"
-      />
+      <Comments />
     </template>
   </Layout>
 </template>
