d045804722ce9e7d5f5652951ec364f22c616d4a
Cody Bontecou <bontecouc@gmail.com>
2023-02-05 20:05:32 -1000

    removed unused imports

--- src/.vitepress/theme/MyLayout.vue
+++ src/.vitepress/theme/MyLayout.vue
@@ -1,7 +1,5 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
-import { useRoute } from 'vitepress'
-import { computed, ref, watch, onMounted } from 'vue'
 
 const { Layout } = DefaultTheme
 </script>
