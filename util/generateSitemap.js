0c6ce5525fc532d4ba70422bb79c524d0c7ff0d3
Cody Bontecou <bontecouc@gmail.com>
2024-08-29 20:58:09 +0800

    updated with import syntax

--- util/generateSitemap.js
+++ util/generateSitemap.js
@@ -1,24 +1,28 @@
-const fs = require('fs')
-const path = require('path')
-const fg = require('fast-glob')
-const { SitemapStream, streamToPromise } = require('sitemap')
+import fs from 'fs';
+import path from 'path';
+import fg from 'fast-glob';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { fileURLToPath } from 'url';
 
-console.log('Start building sitemap..')
+console.log('Start building sitemap..');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const linksStream = fg
   .stream(['**/*.md', '!**/node_modules', '!README.md'])
   .map(filePath => ({
     url: filePath.replace('src/', '').replace(/\.md$/, '.html'),
-  }))
+  }));
 
 const sitemapStream = new SitemapStream({
   hostname: 'https://codybontecou.com/',
-})
+});
 
 // Return a promise that resolves with your XML string
 streamToPromise(linksStream.pipe(sitemapStream)).then(sitemap => {
   fs.writeFileSync(
     path.resolve(__dirname, '../src/public/sitemap.xml'),
     sitemap
-  )
-})
+  );
+});
