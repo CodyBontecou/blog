5520cbf980b28ec247a3c01cafcd21d1f73e6d11
Cody Bontecou <bontecouc@gmail.com>
2024-09-01 11:14:19 +0800

    updated sitemap with www.

--- util/generateSitemap.js
+++ util/generateSitemap.js
@@ -1,28 +1,28 @@
-import fs from 'fs';
-import path from 'path';
-import fg from 'fast-glob';
-import { SitemapStream, streamToPromise } from 'sitemap';
-import { fileURLToPath } from 'url';
+import fs from 'fs'
+import path from 'path'
+import fg from 'fast-glob'
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { fileURLToPath } from 'url'
 
-console.log('Start building sitemap..');
+console.log('Start building sitemap..')
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const linksStream = fg
-  .stream(['**/*.md', '!**/node_modules', '!README.md'])
-  .map(filePath => ({
-    url: filePath.replace('src/', '').replace(/\.md$/, '.html'),
-  }));
+    .stream(['**/*.md', '!**/node_modules', '!README.md'])
+    .map(filePath => ({
+        url: filePath.replace('src/', '').replace(/\.md$/, '.html'),
+    }))
 
 const sitemapStream = new SitemapStream({
-  hostname: 'https://codybontecou.com/',
-});
+    hostname: 'https://www.codybontecou.com/',
+})
 
 // Return a promise that resolves with your XML string
 streamToPromise(linksStream.pipe(sitemapStream)).then(sitemap => {
-  fs.writeFileSync(
-    path.resolve(__dirname, '../src/public/sitemap.xml'),
-    sitemap
-  );
-});
+    fs.writeFileSync(
+        path.resolve(__dirname, '../src/public/sitemap.xml'),
+        sitemap
+    )
+})
