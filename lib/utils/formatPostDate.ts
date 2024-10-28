dfec266fa1f0b1f2143fc6661c7e95e45698f79b
Cody Bontecou <bontecouc@gmail.com>
2024-10-27 21:50:56 -0700

    type

--- lib/utils/formatPostDate.ts
+++ lib/utils/formatPostDate.ts
@@ -1,5 +1,5 @@
 // Format the date for the posts list (YYYY · MM)
-export const formatPostDate = date => {
+export const formatPostDate = (date: string) => {
     const d = new Date(date)
     return `${d.getFullYear()} · ${String(d.getMonth() + 1).padStart(2, '0')}`
 }
