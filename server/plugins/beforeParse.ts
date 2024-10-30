506bc32610a021f801253e22cdc06558477b62c2
Cody Bontecou <bontecouc@gmail.com>
2024-10-30 09:25:22 -0700

    Removing plugin. I think it's breaking the build

--- server/plugins/beforeParse.ts
+++
@@ -1,7 +0,0 @@
-export default defineNitroPlugin(nitroApp => {
-    nitroApp.hooks.hook('content:file:beforeParse', file => {
-        if (file._id.endsWith('.md')) {
-            // console.log(file)
-        }
-    })
-})
