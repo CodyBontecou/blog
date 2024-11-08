8cea272202cdd8e242593b7d9371f8abc8c29993
Cody Bontecou <bontecouc@gmail.com>
2024-11-08 08:01:30 -0800

    cleaned up addToMailchimp function

--- lib/utils/addToMailchimp.ts
+++ lib/utils/addToMailchimp.ts
@@ -1,15 +1,14 @@
 import jsonp from 'jsonp'
 
 export function addToMailchimp(email: string) {
+    const emailEncoded = encodeURIComponent(email)
+
     const dataCenter = 'gmail'
     const userId = 'cd437705ede047b78169e4337'
     const listId = 'c7dcc86f21'
-    const endpoint = `https://${dataCenter}.us10.list-manage.com/subscribe/post?u=${userId}&amp;id=${listId}&amp;f_id=00b0bbe3f0`
+    const endpoint = `https://${dataCenter}.us10.list-manage.com/subscribe/post?u=${userId}&amp;id=${listId}&amp;f_id=00b0bbe3f0&EMAIL=${emailEncoded}`
 
-    const emailEncoded = encodeURIComponent(email)
-    let url = endpoint.replace(/\/post/g, '/post-json')
-    const queryParams = `&EMAIL=${emailEncoded}`
-    url = `${url}${queryParams}`
+    const url = endpoint.replace(/\/post/g, '/post-json')
 
     return new Promise((resolve, reject) =>
         jsonp(url, { param: 'c', timeout: 3500 }, (err, data) => {
