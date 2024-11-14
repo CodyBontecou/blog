c73bf5aec084173eedd19663180a0ff86a46571d
Cody Bontecou <bontecouc@gmail.com>
2024-11-14 00:27:12 -0800

    open to work toast

--- app.vue
+++ app.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import Toaster from '@/components/ui/toast/Toaster.vue'
+import { useToast } from '@/components/ui/toast/use-toast'
+
+const { t } = useI18n()
+const { toast } = useToast()
+
+onMounted(() => {
+    toast({
+        duration: 5000,
+        title: t('work.title'),
+        description: t('work.description'),
+    })
+})
 </script>
 
 <template>
     <div>
-        <NuxtPage />
         <Toaster />
+        <NuxtPage />
     </div>
 </template>
--- components/ui/toast/Toaster.vue
+++ components/ui/toast/Toaster.vue
@@ -1,30 +1,42 @@
 <script setup lang="ts">
 import { isVNode } from 'vue'
-import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from '.'
+import {
+    Toast,
+    ToastClose,
+    ToastDescription,
+    ToastProvider,
+    ToastTitle,
+    ToastViewport,
+} from '.'
 import { useToast } from './use-toast'
 
 const { toasts } = useToast()
 </script>
 
 <template>
-  <ToastProvider>
-    <Toast v-for="toast in toasts" :key="toast.id" v-bind="toast">
-      <div class="grid gap-1">
-        <ToastTitle v-if="toast.title">
-          {{ toast.title }}
-        </ToastTitle>
-        <template v-if="toast.description">
-          <ToastDescription v-if="isVNode(toast.description)">
-            <component :is="toast.description" />
-          </ToastDescription>
-          <ToastDescription v-else>
-            {{ toast.description }}
-          </ToastDescription>
-        </template>
-        <ToastClose />
-      </div>
-      <component :is="toast.action" />
-    </Toast>
-    <ToastViewport />
-  </ToastProvider>
+    <ToastProvider>
+        <Toast
+            class="bg-white"
+            v-for="toast in toasts"
+            :key="toast.id"
+            v-bind="toast"
+        >
+            <div class="grid gap-1">
+                <ToastTitle v-if="toast.title">
+                    {{ toast.title }}
+                </ToastTitle>
+                <template v-if="toast.description">
+                    <ToastDescription v-if="isVNode(toast.description)">
+                        <component :is="toast.description" />
+                    </ToastDescription>
+                    <ToastDescription v-else>
+                        {{ toast.description }}
+                    </ToastDescription>
+                </template>
+                <ToastClose />
+            </div>
+            <component :is="toast.action" />
+        </Toast>
+        <ToastViewport />
+    </ToastProvider>
 </template>
