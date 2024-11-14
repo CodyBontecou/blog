e996c8849203e06d62b756a410bd024db447f2de
Cody Bontecou <bontecouc@gmail.com>
2024-11-14 08:22:00 -0800

    does this fix the 500?

--- middleware/html-redirect.global.ts
+++ middleware/html-redirect.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(to => {
-    if (to.fullPath.endsWith('.html')) {
-        return navigateTo(to.fullPath.slice(0, -5), { redirectCode: 301 })
-    }
+    // if (to.fullPath.endsWith('.html')) {
+    //     return navigateTo(to.fullPath.slice(0, -5), { redirectCode: 301 })
+    // }
 })
