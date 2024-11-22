548122438f7e89bc824c75007fbcf2ef861358cd
Cody Bontecou <bontecouc@gmail.com>
2024-11-21 16:15:27 -0800

    vault backup: 2024-11-21 16:15:27

--- pages/about.vue
+++ pages/about.vue
@@ -9,19 +9,77 @@
                     <div class="mb-12">
                         <h1 class="text-2xl font-normal mb-4">About me</h1>
                         <p>
-                            I’m Cody. You may also know me as bonteq, currently
-                            enjoying life. Previously I co-founded
-                            <a href="https://www.i18nlocale.com">i18nLocale</a>
+                            I’m Cody. I'm currently enjoying life. Previously I
+                            co-founded
+                            <a
+                                class="underline"
+                                href="https://www.i18nlocale.com"
+                                >i18nLocale</a
+                            >
                             and
-                            <a href="https://www.xfeedmuter.com">xFeedMuter</a>.
+                            <a
+                                class="underline"
+                                href="https://www.xfeedmuter.com"
+                                >xFeedMuter</a
+                            >.
                         </p>
 
-                        <ul class="mt-4">
-                            A few links to explore:
-                            <li><a href="">Writing</a></li>
-                            <li><a href="">Projects</a></li>
-                            <li><a href="">Talks</a></li>
-                            <li><a href="">What I'm doing now</a></li>
+                        <h2 class="my-4">A few links to explore:</h2>
+                        <ul class="list-disc list-inside space-y-1 underline">
+                            <li>
+                                <NuxtLink to="/">Writing</NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink to="/">Projects</NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink to="/">Talks</NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink to="/">What I'm doing now</NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="mb-12">
+                        <h2 class="text-lg font-normal">Elsewhere</h2>
+                        <p class="my-4">
+                            You can
+                            <NuxtLink class="underline" to="subscribe">
+                                receive updates from me via email
+                            </NuxtLink>
+                            or follow me:
+                        </p>
+
+                        <ul class="list-disc list-inside space-y-1">
+                            <li>
+                                RSS feed:
+                                <a class="underline" href="">
+                                    stephango.com/feed
+                                </a>
+                            </li>
+                            <li>
+                                Twitter:
+                                <a class="underline" href="">@codybontecou</a>
+                            </li>
+                            <li>
+                                Github:
+                                <a class="underline" href="">@codybontecou</a>
+                            </li>
+                            <li>
+                                Bluesky:
+                                <a class="underline" href="">
+                                    @codybontecou.com
+                                </a>
+                            </li>
+                            <li>
+                                Threads:
+                                <a class="underline" href="">@bonteq</a>
+                            </li>
+                            <li>
+                                Instagram:
+                                <a class="underline" href="">@bonteq</a>
+                            </li>
                         </ul>
                     </div>
                 </main>
---
+++ pages/subscribe.vue
@@ -0,0 +1,1 @@
+<template>subscribe to newsletter</template>
