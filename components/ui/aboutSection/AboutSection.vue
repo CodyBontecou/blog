c7c4ad56e23e0df5f8035d7cddd48b8b90904e32
Cody Bontecou <bontecouc@gmail.com>
2024-11-21 18:57:53 -0800

    built out the about page.

---
+++ components/ui/aboutSection/AboutSection.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+    links: Link[]
+    header: string
+    separate: boolean
+}
+
+interface Link {
+    to: string
+    name: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <h2 class="my-4">{{ header }}</h2>
+    <ul class="list-disc list-inside space-y-1 underline">
+        <li v-for="link in links">
+            <NuxtLink :to="link.to">
+                {{ link.name }}
+            </NuxtLink>
+        </li>
+    </ul>
+
+    <Separator v-if="separate" class="my-10"></Separator>
+</template>
