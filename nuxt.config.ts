0de87251d98dc947d1d872e01779daf54e636a58
Cody Bontecou <bontecouc@gmail.com>
2024-12-12 12:09:55 -0600

    trying strict routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    router: {
+        options: {
+            strict: true, // Enforce strict routing (optional)
+        },
+    },
     devtools: {
         enabled: true,
         timeline: {
