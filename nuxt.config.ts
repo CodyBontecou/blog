0a06185301be210861e60f313c43e6bac9ec4797
Cody Bontecou <bontecouc@gmail.com>
2024-12-12 12:20:28 -0600

    removing url strict

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    router: {
-        options: {
-            strict: false, // Enforce strict routing (optional)
-        },
-    },
     devtools: {
         enabled: true,
         timeline: {
