cf53a3b87416ed371bc2ade98864d571e66ab637
Cody Bontecou <bontecouc@gmail.com>
2024-12-12 12:28:18 -0600

    back to this version...

--- middleware/removeTrailingSlash.global.ts
+++ middleware/removeTrailingSlash.global.ts
@@ -1,10 +1,8 @@
-export default defineNuxtRouteMiddleware(to => {
-    if (to.path.endsWith('/') && to.path !== '/') {
+export default defineNuxtRouteMiddleware((to, from) => {
+    if (to.path !== '/' && to.path.endsWith('/')) {
         const { path, query, hash } = to
-        const nextPath = `${path}`
-        return navigateTo(
-            { path: nextPath, query, hash },
-            { redirectCode: 301 }
-        )
+        const nextPath = path.replace(/\/+$/, '') || '/'
+        const nextRoute = { path: nextPath, query, hash }
+        return navigateTo(nextRoute, { redirectCode: 301 })
     }
 })
