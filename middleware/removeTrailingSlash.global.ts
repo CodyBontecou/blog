1575e0228e300e40d2d3871c607223e2bf00fde0
Cody Bontecou <bontecouc@gmail.com>
2024-12-12 12:23:37 -0600

    trying without forcing slash

--- middleware/removeTrailingSlash.global.ts
+++ middleware/removeTrailingSlash.global.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware(to => {
-    if (!to.path.endsWith('/') && to.path !== '/') {
+    if (to.path.endsWith('/') && to.path !== '/') {
         const { path, query, hash } = to
-        const nextPath = `${path}/`
+        const nextPath = `${path}`
         return navigateTo(
             { path: nextPath, query, hash },
             { redirectCode: 301 }
