ecfb5ad44c9617ea1a7b90254b45be4cdc2d22a2
Cody Bontecou <bontecouc@gmail.com>
2025-04-13 20:33:18 -0600

    removed excalidraw

--- components/excalidraw.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.excalidraw' {
-    import type { ExcalidrawInitialDataState } from '@excalidraw/excalidraw/types/types'
-    const data: ExcalidrawInitialDataState
-    export default data
-}
